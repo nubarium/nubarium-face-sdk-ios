3b5bd17bb27793f911d3da5c97f135a42f017750
Luis Arriaga <luis.arriaga@nubarium.com>
2025-09-22 18:53:52 -0600

    Se ajusta version binary

--- Package.swift
+++ Package.swift
@@ -3,24 +3,38 @@ import PackageDescription
 
 let package = Package(
     name: "SDKFaceComponent",
-    platforms: [.iOS(.v13)],
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        .library(name: "SDKFaceComponent", targets: ["SDKFaceKit"])
+        // Lo que importará el cliente:  import FaceCapture
+        .library(name: "FaceCapture", targets: ["FaceCapture"])
     ],
     dependencies: [
-        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.0.0")
+        // Lottie 4.x
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.0.0"),
+        .package(url: "https://github.com/bustoutsolutions/siesta.git", from: "1.5.2"),
     ],
     targets: [
-        .binaryTarget(
+        // ===== Opción A: binario LOCAL committeado en el repo =====
+        //.binaryTarget(
+        //    name: "SDKFaceComponent",
+        //    path: "Artifacts/SDKFaceComponent.xcframework"
+        //),
+        // ===== Opción B: binario REMOTO (usa esta si publicas el zip en S3) =====
+         .binaryTarget(
             name: "SDKFaceComponent",
-            url: "https://nubarium-sdk-ios.s3.ca-central-1.amazonaws.com/releases/ios/SDKFaceComponent/v1.0.5/SDKFaceComponent.xcframework.zip",
-            checksum: "685b3ac2dd5826d3f68941180e1a7b3641c98d045da18341d91ac2529fc51605"
+            url: "https://nubarium-sdk-ios.s3.ca-central-1.amazonaws.com/releases/ios/SDKFaceComponent/b45a3e07-78d3-40e4-881c-7d45032e50db/v1.0.7/SDKFaceComponent.xcframework.zip",
+            checksum: "cc77961b3de9087925fd2de97317c81a0eba9a7e0e6c226a9b65170a0960f2f4"
         ),
+
+        // Wrapper que expone el API al cliente e integra Lottie
         .target(
-            name: "SDKFaceKit",
+            name: "FaceCapture",
             dependencies: [
                 .target(name: "SDKFaceComponent"),
-                .product(name: "Lottie", package: "lottie-ios")
+                .product(name: "Lottie", package: "lottie-ios"),
+                .product(name: "Siesta", package: "siesta"),
             ],
             path: "Sources/SDKFaceKit"
         )
